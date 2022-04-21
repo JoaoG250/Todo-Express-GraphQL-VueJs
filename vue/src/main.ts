5b05dc460485df308b4ae8451018d82febe8383c
João Rodrigues <joao.devmail@gmail.com>
2022-04-21 11:31:10 -0300

    feat: apollo client installed

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,10 +1,17 @@
-import { createApp } from "vue";
+import { createApp, h, provide } from "vue";
 import { createPinia } from "pinia";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
 import App from "./App.vue";
 import router from "./router";
+import apolloClient from "./apollo/client";
 
-const app = createApp(App);
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+});
 
 app.use(createPinia());
 app.use(router);
